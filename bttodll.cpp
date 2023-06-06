2f6055f402d7ecb4db9f37d0706f6ea0bfdd1b48
Manan Hingorani <97526579+MananHingorani2003@users.noreply.github.com>
2023-06-06 12:57:57 +0530

    Create bttodll.cpp

---
+++ bttodll.cpp
@@ -0,0 +1,46 @@
+BinaryTreeNode<int>* BTtoDLL(BinaryTreeNode<int>* root) {
+    // Write your code here
+    BinaryTreeNode <int> *curr = root;
+    BinaryTreeNode <int> *head = NULL;
+    BinaryTreeNode <int> *tail = NULL;
+
+    while (curr != NULL) {
+        if (curr -> left == NULL) {
+            if (head == NULL) {
+                head = curr;
+                tail = curr;
+            }
+            else {
+                tail -> right = curr;
+                tail = tail -> right;
+            }
+            curr = curr -> right;
+        }
+        else {
+            BinaryTreeNode <int> *prev = curr->left;
+            while (prev -> right && prev -> right != curr) {
+                prev = prev -> right;
+            }
+            if (prev -> right == NULL) {
+                prev -> right = curr;
+                curr = curr -> left;
+            }
+            else {
+                tail -> right = curr;
+                tail = tail -> right;
+                curr = curr -> right;
+            }
+        }
+    }
+
+    curr = head;
+    BinaryTreeNode <int> *prev2 = NULL;
+    while (curr) {
+        curr->left = prev2;
+        prev2 = curr;
+        curr = curr->right;
+    }
+
+    return head;
+
+}
